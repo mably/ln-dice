beddf0b2b5395005c3daeaec46c0d9414e8a9f64
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-18 13:41:54 +0800

    Delete app directory

--- app/database.js
+++
@@ -1,9 +0,0 @@
-// app/database.js
-var Db = require("tingodb")({ searchInArray: true }).Db;
-
-// expose the routes to our app with module.exports
-module.exports = function (dataPath) {
-
-	return new Db(dataPath, {});
-
-};
